ccaaa028a0e26c326d46b22d135b9f5edc55d9a7
Yam Peled <43607303+yampeled1@users.noreply.github.com>
2018-10-15 15:03:05 +0300

    Update logs_backups3.sh

--- logs_backups3.sh
+++ logs_backups3.sh
@@ -8,5 +8,14 @@ read log
 #Compressing the directory/file and adding time stamp 
 tar -cf "$log.$(date +%F_%R).tar" $log
 
+echo
+#Listing the bucket available
+s3cmd ls
+echo
+
+#Choosing the desired bucket
+echo Please select to which bucket you would like to push the files "(name only)"
+read bckt
+
 #Pushing the backup file/directory to s3 bucket
-s3cmd put -r $log.$(date +%F_%R).tar s3://yams-bucket-exp/
+s3cmd put -r $log.$(date +%F_%R).tar s3://$bckt/
